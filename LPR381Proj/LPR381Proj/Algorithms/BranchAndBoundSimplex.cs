36a23cd2e42d7511c9f132f2633f6399f3e59697
mfield-hub <milenkef@gmail.com>
2026-08-16 13:05:40 +0200

    Added Rough Project Structure
    Added rough project structure

---
+++ LPR381Proj/LPR381Proj/Algorithms/BranchAndBoundSimplex.cs
@@ -0,0 +1,12 @@
+﻿fusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR381Proj.Algorithms
+{
+    internal class BranchAndBoundSimplex
+    {
+    }
+}
